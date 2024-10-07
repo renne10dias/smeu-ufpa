80326e664131daebf59cfe9d882f4ac2c083632c
renne farias dias <rennedias2003@gmail.com>
2024-10-07 18:43:08 -0300

    alguma coisa foi commitada

--- src/api/express/ApiExpress.ts
+++ src/api/express/ApiExpress.ts
@@ -1,27 +1,23 @@
 import { Request, Response } from "express";
-import { MulterConfig } from "../../util/multer/MulterConfig"; 
 
-import cors from 'cors'; // Importa o pacote CORS
+import cors from 'cors';
 
 export class ApiExpress {
-    private static instance: ApiExpress; // Variável estática para armazenar a instância única
-    private routes: { method: string; path: string; handler: Function | Function[] }[] = []; // Aceita uma função ou um array de funções
+    private static instance: ApiExpress;
+    private routes: { method: string; path: string; handler: Function | Function[] }[] = [];
 
-
-    // Construtor privado para evitar a instância externa
     private constructor() {}
 
-    // Método para obter a instância única
     public static getInstance(): ApiExpress {
         if (!ApiExpress.instance) {
-            ApiExpress.instance = new ApiExpress(); // Cria uma nova instância se não existir
+            ApiExpress.instance = new ApiExpress();
         }
-        return ApiExpress.instance; // Retorna a instância única
+        return ApiExpress.instance;
     }
 
-    // Método para adicionar rotas POST com suporte ao upload de arquivos
-    public addPostRoute(path: string, Controller: any, methodName: string, uploadMiddleware?: any) {
-        const controller = Controller.build(); // Instancia o controlador
+    // Definindo tipos para Controller e métodos
+    public addPostRoute<T extends { build: () => any }>(path: string, Controller: T, methodName: string, uploadMiddleware?: any) {
+        const controller = Controller.build();
 
         const handler = async (req: Request, res: Response) => {
             try {
@@ -31,7 +27,6 @@ export class ApiExpress {
             }
         };
 
-        // Se houver middleware para upload de arquivos, usa ele
         if (uploadMiddleware) {
             this.routes.push({ method: 'POST', path, handler: [uploadMiddleware, handler] });
         } else {
@@ -39,11 +34,8 @@ export class ApiExpress {
         }
     }
 
-
-
-    // Método para adicionar rotas GET
-    public addGetRoute(path: string, Controller: any, methodName: string) {
-        const controller = Controller.build(); // Instancia o controlador
+    public addGetRoute<T extends { build: () => any }>(path: string, Controller: T, methodName: string) {
+        const controller = Controller.build();
 
         const handler = async (req: Request, res: Response) => {
             try {
@@ -56,9 +48,8 @@ export class ApiExpress {
         this.routes.push({ method: 'GET', path, handler });
     }
 
-    // Método para adicionar rotas PUT
-    public addPutRoute(path: string, Controller: any, methodName: string) {
-        const controller = Controller.build(); // Instancia o controlador
+    public addPutRoute<T extends { build: () => any }>(path: string, Controller: T, methodName: string) {
+        const controller = Controller.build();
 
         const handler = async (req: Request, res: Response) => {
             try {
@@ -71,9 +62,8 @@ export class ApiExpress {
         this.routes.push({ method: 'PUT', path, handler });
     }
 
-    // Método para adicionar rotas DELETE
-    public addDeleteRoute(path: string, Controller: any, methodName: string) {
-        const controller = Controller.build(); // Instancia o controlador
+    public addDeleteRoute<T extends { build: () => any }>(path: string, Controller: T, methodName: string) {
+        const controller = Controller.build();
 
         const handler = async (req: Request, res: Response) => {
             try {
@@ -86,20 +76,14 @@ export class ApiExpress {
         this.routes.push({ method: 'DELETE', path, handler });
     }
 
-     // Método para iniciar o servidor e registrar as rotas
-     public start(port: number) {
+    public start(port: number) {
         const express = require('express');
         const app = express();
 
-        // Middleware para parsing de JSON
         app.use(express.json());
-
-        // Middleware para habilitar o CORS
         app.use(cors());
 
-        // Adiciona as rotas ao express
         this.routes.forEach(route => {
-            // Usa o método express e passa o handler como função ou array de funções
             if (Array.isArray(route.handler)) {
                 app[route.method.toLowerCase()](route.path, ...route.handler);
             } else {
@@ -111,8 +95,4 @@ export class ApiExpress {
             console.log(`Server running on port ${port}`);
         });
     }
-
-
-
-    
 }
